8617b47a200c625792e1e2cdb881d98b4b8c65f6
yamzal1 <yannis.amzal@u-psud.fr>
2022-05-30 19:34:25 +0200

    Correction overflow page details theme

--- lib/pages/details.dart
+++ lib/pages/details.dart
@@ -75,84 +75,83 @@ class _DetailsState extends State<Details> {
             Column(
               children: [
                 const SizedBox(height: 24),
-                Container(
-                  child: ValueListenableBuilder(
-                    //permet de construire la liste en récupérant les données de la box. Les lignes seront mises les unes à la suite des autres, par ordre croissant d'id
-                    valueListenable: soundBox.listenable(),
-                    builder: (context, Box box, widget) {
-                      if (box.isEmpty) {
-                        return Center(
-                          child: Text('Le thème ne contient aucun son'),
-                        );
-                      } else {
-                        return SizedBox(
-                          width: 600,
-                          child: ListView.builder(
-                            shrinkWrap: true,
-                            itemCount: box.length,
-                            itemBuilder: (context, index) {
-                              var currentBox = box;
-                              var soundData = currentBox.getAt(index)!;
+                ValueListenableBuilder(
+                  //permet de construire la liste en récupérant les données de la box. Les lignes seront mises les unes à la suite des autres, par ordre croissant d'id
+                  valueListenable: soundBox.listenable(),
+                  builder: (context, Box box, widget) {
+                    if (box.isEmpty) {
+                      return Center(
+                        child: Text('Le thème ne contient aucun son'),
+                      );
+                    } else {
+                      return SizedBox(
+                        width: 600,
+                        height: MediaQuery.of(context).size.height * 0.35,
+                        child: ListView.builder(
+                          shrinkWrap: true,
+                          itemCount: box.length,
+                          itemBuilder: (context, index) {
+                            var currentBox = box;
+                            var soundData = currentBox.getAt(index)!;
 
-                              return Card(
-                                  elevation: 8.0,
-                                  margin: new EdgeInsets.symmetric(
-                                      horizontal: 10.0, vertical: 6.0),
-                                  child: Container(
-                                    decoration:
-                                        BoxDecoration(color: AppColors.darkGrey),
-                                    child: ListTile(
-                                      contentPadding: EdgeInsets.symmetric(
-                                          horizontal: 20.0, vertical: 10.0),
-                                      leading: Container(
-                                        padding: EdgeInsets.only(right: 12.0),
-                                        decoration: new BoxDecoration(
-                                            border: new Border(
-                                                right: new BorderSide(
-                                                    width: 1.0,
-                                                    color: Colors.white24))),
-                                        //TODO mettre l'icone de la bd
+                            return Card(
+                                elevation: 8.0,
+                                margin: new EdgeInsets.symmetric(
+                                    horizontal: 10.0, vertical: 6.0),
+                                child: Container(
+                                  decoration:
+                                      BoxDecoration(color: AppColors.darkGrey),
+                                  child: ListTile(
+                                    contentPadding: EdgeInsets.symmetric(
+                                        horizontal: 20.0, vertical: 10.0),
+                                    leading: Container(
+                                      padding: EdgeInsets.only(right: 12.0),
+                                      decoration: new BoxDecoration(
+                                          border: new Border(
+                                              right: new BorderSide(
+                                                  width: 1.0,
+                                                  color: Colors.white24))),
+                                      //TODO mettre l'icone de la bd
 
-                                        child: Icon(Icons.airplanemode_active,
-                                            color: Colors.white),
-                                      ),
-                                      title: Text(
-                                        soundData.name,
-                                        style: TextStyle(
-                                            color: Colors.white,
-                                            fontWeight: FontWeight.bold),
-                                      ),
-                                      trailing: Wrap(
-                                        spacing: 12, // space between two icons
-                                        children: <Widget>[
-                                          IconButton(
-                                            //TODO RENVOYER VERS FORMULAIRE MODIFICATION
-                                            onPressed: () => _deleteInfo(index),
-                                            icon: Icon(
-                                              Icons.volume_up,
-                                              color: Colors.blue,
-                                            ),
-                                          ),
-
-                                          IconButton(
-                                            onPressed: () => _deleteInfo(index),
-                                            icon: Icon(
-                                              Icons.delete,
-                                              color: Colors.red,
-                                            ),
-                                          ),
-
-                                          // icon-1
-                                        ],
-                                      ),
+                                      child: Icon(Icons.airplanemode_active,
+                                          color: Colors.white),
                                     ),
-                                  ));
-                            },
-                          ),
-                        );
-                      }
-                    },
-                  ),
+                                    title: Text(
+                                      soundData.name,
+                                      style: TextStyle(
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                    trailing: Wrap(
+                                      spacing: 12, // space between two icons
+                                      children: <Widget>[
+                                        IconButton(
+                                          //TODO RENVOYER VERS FORMULAIRE MODIFICATION
+                                          onPressed: () => _deleteInfo(index),
+                                          icon: Icon(
+                                            Icons.volume_up,
+                                            color: Colors.blue,
+                                          ),
+                                        ),
+
+                                        IconButton(
+                                          onPressed: () => _deleteInfo(index),
+                                          icon: Icon(
+                                            Icons.delete,
+                                            color: Colors.red,
+                                          ),
+                                        ),
+
+                                        // icon-1
+                                      ],
+                                    ),
+                                  ),
+                                ));
+                          },
+                        ),
+                      );
+                    }
+                  },
                 ),
               ],
             ),
